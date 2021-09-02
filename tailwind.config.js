d02ac7419331d2c45efe544ff9f454cfda726840
unknown <christlegacies@gmail.com>
2021-09-02 23:13:13 +0700

    add orange color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: [
     './pages/**/*.js',
@@ -5,6 +7,21 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      ...defaultTheme.colors,
+      orange: {
+        50: '#fcefe9',
+        100: '#faded3',
+        200: '#f5bda7',
+        300: '#ef9d7b',
+        400: '#ea7c4f',
+        500: '#e55b23',
+        600: '#ce5220',
+        700: '#b7491c',
+        800: '#a04019',
+        900: '#893715',
+      }
+    },
     extend: {},
   },
   variants: {
