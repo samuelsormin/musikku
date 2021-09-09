44bece441f8ea609eb0403568e498feaf2d69462
unknown <christlegacies@gmail.com>
2021-09-09 11:22:19 +0700

    fetch & show musikku api data from mock.io

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
-import Head from 'next/head';
-import FooterMenu from '../components/FooterMenu';
-import Search from '../components/Search';
+import React from "react";
+import Head from "next/head";
+import FooterMenu from "../components/FooterMenu";
+import Search from "../components/Search";
+import MusicList from "../components/MusicList";
+import AudioHandler from "../helpers/AudioHandler";
 
-export default function Home() {
+export async function getServerSideProps(context) {  
+  const res = await fetch("https://6139752e1fcce10017e78afb.mockapi.io/api/v1/musics")
+  .then(response => response.json());
+  const musics = res;
+
+  return {
+    props: { musics }
+  }
+}
+
+export default function Home({musics}) {
+  const [playerHandler, controlObj, setControlObj] = AudioHandler();
+  
   return (
     <>
       <Head>
@@ -17,49 +32,18 @@ export default function Home() {
         <div className="mx-5 pt-5">
           <Search placeholder="Search downloaded musics" />
         </div>
-        <div className="mx-5 pt-8 space-y-3">
-          <div className="relative">
-            <div className="flex bg-gray-50 rounded-xl p-2 border border-gray-100 w-11/12">
-              <div className="w-16 h-16 rounded-xl bg-gray-300 mr-3"></div>
-              <div className="flex-grow">
-                <p className="font-bold">Losing My Religion</p>
-                <p className="text-sm text-orange-500">R.E.M</p>
-                <p className="text-sm text-gray-500">15 Mei 2009 • 5.02min</p>
-              </div>
-            </div>
-            <div className="absolute right-0 top-3">
-              <div className="flex w-16 h-14 items-center justify-center rounded-xl bg-orange-500">
-                <svg xmlns="http://www.w3.org/2000/svg" className="flex-none h-12 text-gray-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-                {/* <svg xmlns="http://www.w3.org/2000/svg" className="flex-none h-12 text-gray-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg> */}
-              </div>
-            </div>
-          </div>
-          <div className="relative">
-            <div className="flex bg-gray-50 rounded-xl p-2 border border-gray-100 w-11/12">
-              <div className="w-16 h-16 rounded-xl bg-gray-300 mr-3"></div>
-              <div className="">
-                <p className="font-bold">Losing My Religion</p>
-                <p className="text-sm text-orange-500">R.E.M</p>
-                <p className="text-sm text-gray-500">15 Mei 2009 • 5.02min</p>
-              </div>
-            </div>
-            <div className="absolute right-0 top-3">
-              <div className="flex w-16 h-14 items-center justify-center rounded-xl bg-orange-500">
-                {/* <svg xmlns="http://www.w3.org/2000/svg" className="flex-none h-12 text-gray-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg> */}
-                <svg xmlns="http://www.w3.org/2000/svg" className="flex-none h-12 text-gray-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-              </div>
-            </div>
-          </div>
+        <div className="mx-5 pt-8 space-y-3">          
+          {musics ? musics.map((result, index) => (
+              <MusicList
+              key={result.id.videoId+index}
+              music={result}
+              action="stream"
+              controlObj={controlObj}
+              playerHandler={playerHandler}
+              />
+            )):
+              ''
+            }          
         </div>
       </main>      
       <FooterMenu activeMenu="home" />
